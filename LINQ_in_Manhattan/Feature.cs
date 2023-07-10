6f858e8cbdcd205a6379fd42f3cc55f656a00d66
ph-alaa <Alaa_lafi333@hotmail.com>
2023-07-11 02:06:05 +0300

    lab09

---
+++ LINQ_in_Manhattan/Feature.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_in_Manhattan
+{
+    public class Feature
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("geometry")]
+        public Geometry Geometry { get; set; }
+
+        [JsonProperty("properties")]
+        public Properties Properties { get; set; }
+    }
+}
---
+++ LINQ_in_Manhattan/Root.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_in_Manhattan
+{
+    public class Root
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("features")]
+        public List<Feature> Features { get; set; }
+    }
+}
+
