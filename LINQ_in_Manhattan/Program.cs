21c9872c4fe5be5f7477da904a05be256390d7fb
ph-alaa <Alaa_lafi333@hotmail.com>
2023-07-11 00:03:42 +0300

    Add project files.

---
+++ LINQ_in_Manhattan/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_in_Manhattan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
